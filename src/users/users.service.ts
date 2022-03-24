d3c2b2d9224afabf33ffa321269995ee47749513
Diamond <nataniel.r.v@gmail.com>
2022-03-24 12:18:07 -0300

    creating CRUD service methods

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -11,5 +11,29 @@ export class UsersService {
         },
     ];
 
-    
+    findAll() {
+        return this.users;
+    }
+
+    findOne(id: string) {
+        return this.users.find((user) => user.id === Number(id))
+    }
+
+    create(userDataDto: User) {
+        this.users.push(userDataDto);
+    }
+
+    update(id: string, userDataDto: User) {
+        const index = this.users.findIndex(user => user.id === Number(id));
+
+        this.users[index] = userDataDto;
+    }
+
+    remove(id: string) {
+        const index = this.users.findIndex(user => user.id === Number(id));
+
+        if (index >= 0) {
+            this.users.splice(index, 1);
+        }
+    }
 }
