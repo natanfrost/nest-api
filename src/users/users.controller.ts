8d42736593398b8baa3a959e1de92b1119fa4966
Diamond <nataniel.r.v@gmail.com>
2022-03-24 10:28:23 -0300

    added prefix list  to users get request

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get } from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
-    @Get()
+    @Get('list')
     findAll() {
         return 'users list';
     }
