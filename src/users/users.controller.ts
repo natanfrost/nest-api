2ead545ee562395d195d00e1934bb9fa213677e6
Diamond <nataniel.r.v@gmail.com>
2022-03-24 11:43:01 -0300

    created User entity

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,9 @@
 import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Patch, Post, Res } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+    constructor(private readonly usersService: UsersService) {}
     @Get('list')
     findAll(@Res() response) {
         return response.status(HttpStatus.OK).send('users list');
