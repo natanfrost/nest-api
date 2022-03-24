a4a4b49b1abfb1e7f3289ae347469b84a9b8043f
Diamond <nataniel.r.v@gmail.com>
2022-03-24 10:39:32 -0300

    added post method to users route to simple return the body informed

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
@@ -6,8 +6,14 @@ export class UsersController {
     findAll() {
         return 'users list';
     }
+
     @Get(':id')
     findOne(@Param('id') id: string) {
         return `user number ${id}`
+    }
+
+    @Post()
+    create(@Body() body) {
+        return body;
     }
 }
