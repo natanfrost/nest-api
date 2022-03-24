a89052354b61639f74b8cb0ec62f329066b3ea28
Diamond <nataniel.r.v@gmail.com>
2022-03-24 11:47:07 -0300

    creating array of entntiy user to mocking data

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { User } from './entities/user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    private users: User[] = [
+        {
+            id: 1,
+            name: 'Robert Garcia',
+            tags: ['node.js', 'nest.js', 'javascript']
+        },
+    ];
+
+}
